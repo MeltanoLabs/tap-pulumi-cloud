930a74ff4c68cb9495450f5d93b8b0de9abc8d6f
Edgar R. M <edgarrm358@gmail.com>
2023-04-12 19:52:46 -0600

    chore: Add streams for org members and teams (#3)
    * chore: Add streams for org members and teams
    
    * Ignore empty stream

--- tap_pulumi_cloud/streams.py
+++ tap_pulumi_cloud/streams.py
@@ -2,12 +2,23 @@
 
 from __future__ import annotations
 
+import typing as t
+
 from singer_sdk import typing as th
 
 from tap_pulumi_cloud.client import PulumiCloudStream
 
 
-class Stacks(PulumiCloudStream):
+class _OrgPartitionedStream(PulumiCloudStream):
+    """Base class for streams that are partitioned by organization."""
+
+    @property
+    def partitions(self) -> list[dict] | None:
+        """List of organizations to sync."""
+        return [{"org_name": org} for org in self.config["organizations"]]
+
+
+class Stacks(_OrgPartitionedStream):
     """Users stream."""
 
     name = "stacks"
@@ -42,6 +53,27 @@ class Stacks(PulumiCloudStream):
             description="The number of resources in the stack.",
         ),
     ).to_dict()
+
+    def get_url_params(
+        self,
+        context: dict | None,
+        next_page_token: str | None,
+    ) -> dict[str, t.Any]:
+        """Get URL query parameters.
+
+        Args:
+            context: Stream sync context.
+            next_page_token: Next offset.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params = super().get_url_params(context, next_page_token)
+
+        if context:
+            params["organization"] = context["org_name"]
+
+        return params
 
     def get_child_context(
         self,
@@ -132,4 +164,112 @@ class StackUpdates(PulumiCloudStream):
         ),
         th.Property("resource_changes", th.ObjectType()),
         th.Property("resource_count", th.IntegerType),
+    ).to_dict()
+
+
+class OrganizationMembers(_OrgPartitionedStream):
+    """Organization members stream."""
+
+    name = "organization_members"
+    path = "/api/orgs/{org_name}/members"
+    records_jsonpath = "$.members[*]"
+
+    schema = th.PropertiesList(
+        th.Property(
+            "org_name",
+            th.StringType,
+            description="The name of the organization that owns the stack.",
+        ),
+        th.Property(
+            "role",
+            th.StringType,
+            description="The role of the user in the organization.",
+        ),
+        th.Property(
+            "user",
+            th.ObjectType(
+                th.Property(
+                    "name",
+                    th.StringType,
+                    description="The name of the user.",
+                ),
+                th.Property(
+                    "github_login",
+                    th.StringType,
+                    description="The GitHub login of the user.",
+                ),
+                th.Property(
+                    "avatar_url",
+                    th.StringType,
+                    description="The URL of the user's avatar.",
+                ),
+            ),
+            description="The user object.",
+        ),
+        th.Property(
+            "created",
+            th.DateTimeType,
+            description="The time the user was added to the organization.",
+        ),
+        th.Property(
+            "known_to_pulumi",
+            th.BooleanType,
+        ),
+        th.Property(
+            "virtual_admin",
+            th.BooleanType,
+        ),
+    ).to_dict()
+
+    def get_url_params(
+        self,
+        context: dict | None,
+        next_page_token: str | None,
+    ) -> dict[str, t.Any]:
+        """Return a dictionary of URL query parameters.
+
+        Args:
+            context: The stream sync context.
+            next_page_token: A token for the next page of results.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params = super().get_url_params(context, next_page_token)
+        params["type"] = "backend"
+        return params
+
+
+class OrganizationTeams(_OrgPartitionedStream):
+    """Organization teams stream."""
+
+    name = "organization_teams"
+    path = "/api/orgs/{org_name}/teams"
+    records_jsonpath = "$.teams[*]"
+
+    schema = th.PropertiesList(
+        th.Property(
+            "org_name",
+            th.StringType,
+        ),
+        th.Property(
+            "kind",
+            th.StringType,
+        ),
+        th.Property(
+            "name",
+            th.StringType,
+        ),
+        th.Property(
+            "display_name",
+            th.StringType,
+        ),
+        th.Property(
+            "description",
+            th.StringType,
+        ),
+        th.Property(
+            "user_role",
+            th.StringType,
+        ),
     ).to_dict()
