ca24b911c8d8aa3c68c67927cd9b432dea284091
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-03-03 19:51:45 -0600

    ci: Test with Python 3.12 (#122)

--- noxfile.py
+++ noxfile.py
@@ -21,8 +21,8 @@ package = "tap-pulumi-cloud"
 src_dir = "tap_pulumi_cloud"
 tests_dir = "tests"
 
-python_versions = ["3.11", "3.10", "3.9", "3.8"]
-main_python_version = "3.11"
+python_versions = ["3.12", "3.11", "3.10", "3.9", "3.8"]
+main_python_version = "3.12"
 locations = src_dir, tests_dir, "noxfile.py"
 nox.options.sessions = ("tests",)
 
